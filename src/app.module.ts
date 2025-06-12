5caedeb70cac6797a37846e46e1a6b67c53e15cf
Jaimito27 <softjaimito27@gmail.com>
2025-06-12 12:08:31 -0500

    Se crea el modulo de agentes de transito y se configura el uso global de Pipes para la transformación de datos que vayan entrando

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AgentesModule } from './agentes/agentes.module';
 
 @Module({
-  imports: [],
+  imports: [AgentesModule],
   controllers: [AppController],
   providers: [AppService],
 })

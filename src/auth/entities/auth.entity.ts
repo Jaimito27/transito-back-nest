6e68ad0d011751cf274b7c5d6afa600dd9e60fcb
Jaimito27 <softjaimito27@gmail.com>
2025-06-18 17:18:41 -0500

    Se modela entidad y se inserta el modulo de TypeOrm para crear la tabla en la base de datos. Se instala JWT y se Haseo de contraseñas

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,1 +1,28 @@
-export class Auth {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('usuario')
+export class Auth {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({unique: true})
+    username: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    apellido: string;
+
+    @Column({default: 'user'})
+    rol: string;
+
+    @Column({default: true})
+    estado: boolean;
+
+    @CreateDateColumn()
+    fechaCreacion: Date;
+}
